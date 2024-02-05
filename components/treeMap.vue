9aff0a99a649ecc7aa5aa23c7905c1de3a94b275
salernoelia <putopet@gmail.com>
2024-02-05 21:33:37 +0100

    toggle, almost working

--- components/treeMap.vue
+++ components/treeMap.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <!-- Create a div where the graph will take place -->
+    <div class="controls">
+      <button
+        v-if="currentStorage"
+        type="button"
+        @click="changeView"
+        value="true"
+      >
+        Current Storage
+      </button>
+      <button
+        v-if="!currentStorage"
+        type="button"
+        @click="changeView"
+        value="false"
+      >
+        Potential Storage
+      </button>
+    </div>
+
     <div class="canvas" ref="canvas"></div>
   </div>
 </template>
@@ -13,6 +31,13 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import data from "~/static/data/data.json";
 
 const canvas = ref(null);
+let currentStorage = ref(true);
+let rectangles = [];
+let nodes = [];
+let animate;
+let renderer;
+let scene;
+let camera;
 
 onMounted(async () => {
   // set the dimensions and margins of the graph
@@ -38,7 +63,7 @@ onMounted(async () => {
   // append the svg object to the body of the page
   const svg = d3
     .select(canvas.value)
-    .append("svg")
+    // .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -68,52 +93,73 @@ onMounted(async () => {
       .attr("height", (d) => d.y1 - d.y0);
 
     // Read out d3.js generated rectangles and import them to Three.js
-    const rectangles = svg.selectAll("rect").nodes();
+    rectangles = svg.selectAll("rect").nodes();
 
     // Apply the layout to our data
-    const nodes = root.descendants();
+    nodes = root.descendants();
 
     create3D(rectangles, nodes);
     console.log(rectangles);
+    return { rectangles, nodes };
   } catch (error) {
     console.error("Error loading JSON data:", error);
   }
 });
 
-const colors = [0x2194ce, 0xff6347, 0x8a2be2, 0x00fa9a, 0xffd700];
+function changeView() {
+  console.log("currentStorage:", currentStorage);
+  currentStorage.value = !currentStorage.value;
+  renderer.render(scene, camera);
+}
 
 const create3D = (rectangles, nodes) => {
   // Create a Three.js scene
-  const scene = new THREE.Scene();
+  scene = new THREE.Scene();
+
   // Create three.js renderer
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  // Set the pixel ratio for better quality on high-density displays
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  // Append the renderer's DOM element to the canvas div
   canvas.value.appendChild(renderer.domElement);
 
+  // Add axes helpers
   const axesHelper = new THREE.AxesHelper(100);
   scene.add(axesHelper);
   scene.background = new THREE.Color(0xffffff); // Set background color to white
+
+  const centroid = new THREE.Vector3();
+  rectangles.forEach((rectangle) => {
+    centroid.x += parseFloat(rectangle.getAttribute("x"));
+    centroid.y += parseFloat(rectangle.getAttribute("y"));
+    centroid.z += 0; // Assuming z-coordinate is 0 for 2D layout
+  });
+  centroid.divideScalar(rectangles.length);
 
   rectangles.forEach((rectangle, index) => {
     let rectWidth = parseFloat(rectangle.getAttribute("width"));
     let rectHeight = parseFloat(rectangle.getAttribute("height"));
 
-    // Use the depth value from the data for extrusion
-    let rectDepth = nodes[index].data.depth;
+    let rectAbove, rectBelow;
 
-    if (rectDepth === null || rectDepth === undefined) {
-      rectDepth = 1;
+    if (currentStorage) {
+      rectAbove = data[index].Above_ground_current_storage / 100000000;
+      rectBelow = data[index].Below_ground_current_storage / 100000000;
+    } else {
+      rectAbove = data[index].Above_ground_potential_storage / 100000000;
+      rectBelow = data[index].Below_ground_potential_storage / 100000000;
+    }
+    console.log("currentStorage:", currentStorage);
+
+    if (rectAbove === null || rectAbove === undefined) {
+      rectAbove = 1;
     }
 
     console.log("rectWidth:", rectWidth);
     console.log("rectHeight:", rectHeight);
-    console.log("rectDepth:", rectDepth);
+    console.log("rectAbove:", rectAbove);
+    console.log("rectBelow:", rectBelow);
 
-    // Extrude in the positive direction if the value is positive
     const geometryPositive = new THREE.ExtrudeGeometry(
       new THREE.Shape([
         new THREE.Vector2(0, 0),
@@ -122,10 +168,9 @@ const create3D = (rectangles, nodes) => {
         new THREE.Vector2(0, rectHeight),
         new THREE.Vector2(0, 0),
       ]),
-      { depth: rectDepth, bevelEnabled: false }
+      { depth: rectAbove, bevelEnabled: false }
     );
 
-    // Extrude in the negative direction if the value is negative
     const geometryNegative = new THREE.ExtrudeGeometry(
       new THREE.Shape([
         new THREE.Vector2(0, 0),
@@ -134,28 +179,35 @@ const create3D = (rectangles, nodes) => {
         new THREE.Vector2(0, rectHeight),
         new THREE.Vector2(0, 0),
       ]),
-      { depth: Math.abs(rectDepth), bevelEnabled: false }
+      { depth: Math.abs(rectBelow), bevelEnabled: false }
     );
 
-    // Create meshes for positive and negative extrusions
     const material = new THREE.MeshPhongMaterial({
-      color: colors[index % colors.length],
+      color: data[index].color,
     });
     const meshPositive = new THREE.Mesh(geometryPositive, material);
     const meshNegative = new THREE.Mesh(geometryNegative, material);
 
-    // Position the meshes to "kiss" each other
     meshPositive.position.set(
-      parseFloat(rectangle.getAttribute("x")),
-      parseFloat(rectangle.getAttribute("y")),
+      parseFloat(rectangle.getAttribute("x")) - centroid.x,
+      parseFloat(rectangle.getAttribute("y")) - centroid.y,
       0
     );
 
     meshNegative.position.set(
-      parseFloat(rectangle.getAttribute("x")),
-      parseFloat(rectangle.getAttribute("y")),
-      -rectDepth
+      parseFloat(rectangle.getAttribute("x")) - centroid.x,
+      parseFloat(rectangle.getAttribute("y")) - centroid.y,
+      -Math.abs(rectBelow)
     );
+
+    const existingMeshPositive = scene.getObjectByName(`meshPositive${index}`);
+    const existingMeshNegative = scene.getObjectByName(`meshNegative${index}`);
+
+    if (existingMeshPositive) scene.remove(existingMeshPositive);
+    if (existingMeshNegative) scene.remove(existingMeshNegative);
+
+    meshPositive.name = `meshPositive${index}`;
+    meshNegative.name = `meshNegative${index}`;
 
     // Add meshes to the scene
     scene.add(meshPositive);
@@ -163,37 +215,39 @@ const create3D = (rectangles, nodes) => {
   });
 
   // Add ambient light with increased intensity and white color
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8); // Increase intensity and set color to white
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
   scene.add(ambientLight);
 
   // Add directional light with increased intensity and white color
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2); // Increase intensity and set color to white
-  directionalLight.position.set(1, 1, 1).normalize(); // Adjust position
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
+  directionalLight.position.set(1, 1, 1).normalize();
   scene.add(directionalLight);
 
   // Create a camera
-  const camera = new THREE.PerspectiveCamera(
+  camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    10000
+    20000
   );
-  camera.position.set(0, 0, 200); // Adjust camera position
+  camera.position.set(0, 0, 1500);
 
-  // Rotate the scene so it is flat on X and Y axis
   scene.rotation.x = -Math.PI / 3;
 
-  // Set up OrbitControls with additional features
   const controls = new OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-  controls.dampingFactor = 0.25; // setting damping factor for smoother motion
-  controls.screenSpacePanning = false; // enable/disable screen-space panning
-  controls.maxPolarAngle = Math.PI / 2; // limit the polar angle to avoid going upside down
 
-  controls.minPolarAngle = 0; // Adjust as needed
-  controls.maxPolarAngle = Math.PI; // Adjust as needed
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.25;
+  controls.screenSpacePanning = false;
+  controls.maxDistance = 15000;
 
-  const animate = function () {
+  controls.minPolarAngle = 0;
+  controls.maxPolarAngle = Math.PI;
+
+  controls.minAzimuthAngle = -Infinity;
+  controls.maxAzimuthAngle = Infinity;
+
+  animate = function () {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     // Update the OrbitControls
@@ -206,11 +260,31 @@ const create3D = (rectangles, nodes) => {
 </script>
 
 <style scoped>
+body {
+  top: 0;
+  overflow: hidden;
+  overflow-y: hidden;
+  overflow-x: hidden;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 .canvas {
   width: 100%;
   height: 100%;
+  z-index: 2;
   top: 0;
   left: 0;
+  margin: none;
+  padding: none;
   position: absolute;
+}
+.controls {
+  position: relative;
+  top: 0;
+  left: 0;
+  z-index: 3;
 }
 </style>
