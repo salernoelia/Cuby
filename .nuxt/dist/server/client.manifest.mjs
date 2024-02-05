77c2f542ae9d0e3e17442c041c08784a6cf553ac
salernoelia <putopet@gmail.com>
2024-02-05 16:12:34 +0100

    switch to vue composition api

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,18 @@
+export default {
+  "@vite/client": {
+    "prefetch": true,
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [],
+    "module": true,
+    "resourceType": "script"
+  },
+  "/Users/eliasalerno/GitHub/Cuby/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "isEntry": true,
+    "file": "/Users/eliasalerno/GitHub/Cuby/node_modules/nuxt/dist/app/entry.js"
+  }
+}
