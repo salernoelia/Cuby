b85d0ae973d696d67bd0e18b4332bdbc6ab044ac
salernoelia <putopet@gmail.com>
2024-02-20 19:26:10 +0100

    Simple Subpage implementation

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,7 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <div>
+      <div>
+        <div>
+          <h1>{{ selectedData.Habitat_name }}</h1>
+          <p>{{ selectedData.Habitat_type }}</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import * as d3 from "d3";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+import data from "~/static/data/data.json";
+import { userStore } from "../store/ustore.js";
+
+let selectedData = ref(null);
+
+const ustore = userStore();
+
+selectedData = data.find((p) => p.id === ustore.clickedData);
+</script>
 
 <style lang="scss" scoped></style>
