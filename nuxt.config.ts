2710a444e7b4301b0e17c0469507e7aa5cee2e23
salernoelia <putopet@gmail.com>
2024-03-18 14:04:21 +0100

    fix: idk really

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   ssr: false,
   modules: ["@pinia/nuxt"],
   css: ["/assets/css/global.scss"],
